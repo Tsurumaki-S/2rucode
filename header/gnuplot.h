0cccf4c9c90cd87035918f5729df235e9b7fd90c
Tsurumaki-s <tsurumaki.shuhei@d.mbox.nagoya-u.ac.jp>
2020-01-06 18:19:01 +0900

    modify file.cpp and file.h

---
+++ header/gnuplot.h
@@ -0,0 +1,12 @@
+#ifndef __GNUPLOT__
+#define __GNUPLOT__
+#include "const.h"
+#include "run_manager.h"
+
+class Gnuplot
+{
+	public:
+		void make_Graph_Vector(Run_Manager mr, FileIO fp) ;
+} ;
+
+#endif
--- src/file.cpp
+++ src/file.cpp
@@ -4,21 +4,27 @@
 
 FileIO::FileIO()
 {
-	input1_check  = 0 ;
-	input2_check  = 0 ;
-	output1_check = 0 ;
-	output2_check = 0 ;
-	input1_line_num = -1 ;
-	input2_line_num = -1 ;
+	for(int i=0; i<OPEN_FILE_NUM; i++)
+	{
+		input_check_open[i] = false ;
+		output_check_open[i] = false ;
+		input_line_num[i] = -1 ;
+		output_line_num[i] = -1 ;
+	}
 }
 
-bool FileIO::set_Input_File1(const char *file_name)
+FileIO::~FileIO()
 {
-	input1_line_num = FileIO::setLine(file_name) ;
+	FileIO::close_Files() ;
+}
 
-	if( input1=fopen(file_name,"r") )
+bool FileIO::set_Input_File(int filenum, const char *file_name)
+{
+	input_line_num[filenum] = FileIO::setLine(file_name) ;
+
+	if( fp_input[filenum]=fopen(file_name,"r") )
 	{
-		input1_check = 1 ;
+		input_check_open[filenum] = 1 ;
 		return true ;
 	}
 	else
@@ -28,40 +34,11 @@ bool FileIO::set_Input_File1(const char *file_name)
 	}
 }
 
-bool FileIO::set_Input_File2(const char *file_name)
+bool FileIO::set_Output_File(int filenum, const char *file_name)
 {
-	input2_line_num = setLine(file_name) ;
-
-	if( input2=fopen(file_name,"r") )
+	if( fp_output[filenum]=fopen(file_name,"w") )
 	{
-		input2_check = 1 ;
-		return true ;
-	}
-	else
-	{
-		fprintf(stderr,"\nERROR!\nCan not open the file \"%s\"\n",file_name) ;
-		exit(1) ;
-	}
-}
-
-bool FileIO::set_Output_File1(const char *file_name)
-{
-	if( output1=fopen(file_name,"w") )
-	{
-		output1_check = 1 ;
-		return true ;
-	}
-	else
-	{
-		fprintf(stderr,"\nERROR!\nCan not open the file \"%s\"\n",file_name) ;
-		exit(1) ;
-	}
-}
-bool FileIO::set_Output_File2(const char *file_name)
-{
-	if( output2=fopen(file_name,"w") )
-	{
-		output2_check = 1 ;
+		output_check_open[filenum] = 1 ;
 		return true ;
 	}
 	else
@@ -73,58 +50,37 @@ bool FileIO::set_Output_File2(const char *file_name)
 
 bool FileIO::close_Files()
 {
-	if(input1_check != 0)
+	for(int filenum=0; filenum<OPEN_FILE_NUM; filenum++)
 	{
-		fclose(input1) ;
-		input1_check = 0 ;
+		if(input_check_open[filenum])
+		{
+			fclose(fp_input[filenum]) ;
+			input_check_open[filenum] = false ;
+		}
+
+		if(output_check_open[filenum])
+		{
+			fclose(fp_output[filenum]) ;
+			output_check_open[filenum] = false ;
+		}
 	}
-	if(input2_check != 0)
-	{
-		fclose(input2) ;
-		input2_check = 0 ;
-	}
-	if(output1_check != 0)
-	{
-		fclose(output1) ;
-		output1_check = 0 ;
-	}
-	if(output2_check != 0)
-	{
-		fclose(output2) ;
-		output2_check = 0 ;
-	}
+
 	return true ;
 }
 
-FILE* FileIO::get_Input_File1()
+FILE* FileIO::get_Input_File(int filenum)
 {
-	if(input1_check != 0)
+	if(input_check_open[filenum])
 	{
-		return input1 ;
+		return fp_input[filenum] ;
 	}
 }
 
-FILE* FileIO::get_Input_File2()
+FILE* FileIO::get_Output_File(int filenum)
 {
-	if(input2_check != 0)
+	if(output_check_open[filenum])
 	{
-		return input2 ;
-	}
-}
-
-FILE* FileIO::get_Output_File1()
-{
-	if(output1_check != 0)
-	{
-		return output1 ;
-	}
-}
-
-FILE* FileIO::get_Output_File2()
-{
-	if(output2_check != 0)
-	{
-		return output2 ;
+		return fp_output[filenum] ;
 	}
 }
 
@@ -160,12 +116,8 @@ int FileIO::setLine(const char *file_name)
 
 } ;
 
-int FileIO::get_LineNum_Input_File1()
+int FileIO::get_LineNum_Input_File(int filenum)
 {
-	return input1_line_num ;
+	return input_line_num[filenum] ;
 }
 
-int FileIO::get_LineNum_Input_File2()
-{
-	return input2_line_num ;
-}
