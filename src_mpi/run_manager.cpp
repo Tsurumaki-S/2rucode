4437f0b8ce6f32d19a07afd87ce9e7290a905562
Tsurumaki-s <tsurumaki.shuhei@d.mbox.nagoya-u.ac.jp>
2020-02-07 10:33:20 +0900

    mod comments

--- src_mpi/run_manager.cpp
+++ src_mpi/run_manager.cpp
@@ -6,6 +6,7 @@
 #include "../header/calc_ave_force_in_domain.h"
 #include "../header/format_gro.h"
 
+/* copy arguments */
 Run_Manager::Run_Manager(int argc, char **argv)
 {
 	argument = ALLOC_MATRIX(char,argc,BUFMAX_CHAR) ;
@@ -15,11 +16,15 @@ Run_Manager::Run_Manager(int argc, char **argv)
 	}
 }
 
+/* select analysis mode */
 bool Run_Manager::loadAnalyMode(const char *mode_char) {
 
-    if (analy_tag.converter.find(mode_char) != analy_tag.converter.end()) {
+    if (analy_tag.converter.find(mode_char) != analy_tag.converter.end())
+	{
         mode = analy_tag.converter[mode_char];
-    } else {
+    }
+	else
+	{
         printf("\n  Error: Incorrect argument.\n\n");
 		exit(1) ;
     }
@@ -27,6 +32,7 @@ bool Run_Manager::loadAnalyMode(const char *mode_char) {
     return true;
 }
 
+/* execute analysis which is selected by "loadAnalyMode" */
 bool Run_Manager::doAnaly(Run_Manager mr, FileIO fp)
 {
 	if(mode==analy_tag.converter["avefrc_indomain"])
@@ -64,6 +70,7 @@ bool Run_Manager::doAnaly(Run_Manager mr, FileIO fp)
 	return true ;
 }
 
+/* debug */
 bool Run_Manager::enum_test_print()
 {
 
