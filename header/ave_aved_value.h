938a9c911ce73c2f4bc461943d2d8337c18edba5
Tsurumaki-s <tsurumaki.shuhei@d.mbox.nagoya-u.ac.jp>
2020-03-03 19:07:06 +0900

    add ave_aved_value

---
+++ header/ave_aved_value.h
@@ -0,0 +1,14 @@
+#ifndef __AVE_AVED_VALUE__
+#define __AVE_AVED_VALUE__
+#include "const.h"
+#include "run_manager.h"
+#include "file.h"
+class Ave_Aved_Value
+{
+
+	public:
+		void do_Ave_Aved_Value(Run_Manager mr, FileIO fp) ;
+
+} ;
+
+#endif
