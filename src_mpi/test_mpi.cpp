1fd2bd66f56ed20e3f534770896d3a7b826204a7
Tsurumaki-s <tsurumaki.shuhei@d.mbox.nagoya-u.ac.jp>
2020-03-05 11:03:04 +0900

    mod mpi_test using MPI_REDUCE

--- src_mpi/test_mpi.cpp
+++ src_mpi/test_mpi.cpp
@@ -44,27 +44,58 @@ void Test_Mpi::do_test(Run_Manager mr, FileIO fp)
 				fprintf(fp_output,"Input file contents :\n%s\n",one_line ) ;
 			}
 
-			for(int i = 0;i<10000000;i++)
-			{
-				int dummy ;
-				dummy = id + i ;
-				dummy = id - i ;
-				dummy = id * i ;
-			}
+			//for(int i = 0;i<10000000;i++)
+			//{
+			//	int dummy ;
+			//	dummy = id + i ;
+			//	dummy = id - i ;
+			//	dummy = id * i ;
+			//}
 		}
-		 /* sleep for 20 seconds */
-		 sleep(20);
-		 //std::this_thread::sleep_for(std::chrono::seconds(20));
+
+		/* test calclation with MPI */
+		int test_calc     = id+1 ;
+		int test_calc_sum = -1 ;
+		MPI_Reduce(&test_calc,&test_calc_sum,1,MPI_INT,MPI_SUM ,0,MPI_COMM_WORLD) ;
+
+		int test_calc_sum__true_value = proc_num*(proc_num+1)/2 ;
+
+		if( id == 0 )
+		{
+			char msg[BUFMAX_CHAR_LARGE] ;
+			int n ;
+
+			n = snprintf(msg,BUFMAX_CHAR_LARGE,"\nMPI TEST : ") ;
+			printf(msg) ; fprintf(fp_output,msg) ;
+
+			if( test_calc_sum == test_calc_sum__true_value )
+			{
+				n = snprintf(msg,BUFMAX_CHAR_LARGE,"OK\n") ;
+				printf(msg) ; fprintf(fp_output,msg) ;
+			}
+			else if( test_calc_sum == -1 )
+			{
+				n = snprintf(msg,BUFMAX_CHAR_LARGE,"ERROR!\n") ;
+				printf(msg) ; fprintf(fp_output,msg) ;
+				n = snprintf(msg,BUFMAX_CHAR_LARGE,"MPI function \"MPI_Reduce\" can't execute normally.\n") ;
+				printf(msg) ; fprintf(fp_output,msg) ;
+			}
+			else
+			{
+				n = snprintf(msg,BUFMAX_CHAR_LARGE,"ERROR!\n") ;
+				printf(msg) ; fprintf(fp_output,msg) ;
+				n = snprintf(msg,BUFMAX_CHAR_LARGE,"MPI function \"MPI_Reduce\" can't execute normally.\n") ;
+				printf(msg) ; fprintf(fp_output,msg) ;
+			}
+
+			printf("Now sleeping for 20 seconds.") ;
+		}
+		
+		/* sleep for 20 seconds */
+		MPI_Barrier(MPI_COMM_WORLD) ;
+		sleep(20);
+		//std::this_thread::sleep_for(std::chrono::seconds(20)); // c++11
 	}
-
-
-//	/* copy the last one line */
-//	for(int i=0;i<1;i++)
-//	{
-//		while(fgetc(fp_input) != '\n');
-//		fgets(one_line,1024,fp_input) ;
-//		fprintf(fp_output,"%s",one_line ) ;
-//	}
 
 	MPI_Barrier(MPI_COMM_WORLD) ;
 
